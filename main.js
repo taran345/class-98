86335f1450121c93b0fb2ea6883b814dd3c85b22
taran345 <116542238+taran345@users.noreply.github.com>
2024-02-04 17:47:38 +0530

    Add files via upload

---
+++ main.js
@@ -0,0 +1,26 @@
+var SpeechRecognition = window.webkitSpeechRecognition;
+
+var recognition = new SpeechRecognition();
+
+function start() {
+    document.getElementById("textbox").innerHTML = "";
+   
+    recognition.start();
+}
+
+
+
+recognition.onresult = function (event) {
+
+    console.log(event);
+
+    var content = event.results[0][0].transcript;
+    console.log(content);
+
+    document.getElementById("textbox").innerHTML = content;
+    if (content == "take my selfie") {
+
+        speak_pc();
+
+    }
+}
